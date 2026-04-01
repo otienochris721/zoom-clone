df367cc488b0a58f68649ee71995d01edd1a3882
junior felix <otienochris721@gmail.com>
2026-04-02 02:09:28 +0300

    Final fix for PeerServer and UI buttons

--- public/script.js
+++ public/script.js
@@ -1,16 +1,14 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
-const myVideo = document.createElement('video');
-myVideo.muted = true;
-
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
-  port: '443',
-  proxied: true
+  port: '443'
 });
 
 let myVideoStream;
+const myVideo = document.createElement('video');
+myVideo.muted = true;
 const peers = {};
 
 navigator.mediaDevices.getUserMedia({
@@ -30,54 +28,14 @@ navigator.mediaDevices.getUserMedia({
 
   socket.on('user-connected', userId => {
     setTimeout(() => {
-      connectToNewUser(userId, stream);
+        connectToNewUser(userId, stream);
     }, 1000);
-  });
-
-  // --- CHAT LOGIC ---
-  let text = $("input");
-  $('html').keydown(function (e) {
-    if (e.which == 13 && text.val().length !== 0) {
-      socket.emit('message', text.val());
-      text.val('');
-    }
-  });
-
-  socket.on("createMessage", message => {
-    $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
-    scrollToBottom();
   });
 });
 
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id);
 });
-
-// --- BUTTON FUNCTIONS ---
-
-const muteUnmute = () => {
-  const enabled = myVideoStream.getAudioTracks()[0].enabled;
-  if (enabled) {
-    myVideoStream.getAudioTracks()[0].enabled = false;
-    setUnmuteButton();
-  } else {
-    setMuteButton();
-    myVideoStream.getAudioTracks()[0].enabled = true;
-  }
-}
-
-const playStop = () => {
-  let enabled = myVideoStream.getVideoTracks()[0].enabled;
-  if (enabled) {
-    myVideoStream.getVideoTracks()[0].enabled = false;
-    setPlayVideo();
-  } else {
-    setStopVideo();
-    myVideoStream.getVideoTracks()[0].enabled = true;
-  }
-}
-
-// --- HELPER FUNCTIONS ---
 
 function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream);
@@ -96,7 +54,25 @@ function addVideoStream(video, stream) {
   videoGrid.append(video);
 }
 
-const scrollToBottom = () => {
-  var d = $('.main__chat_window');
-  d.scrollTop(d.prop("scrollHeight"));
+// Button Logic
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    document.querySelector('.main__mute_button').innerHTML = "Unmute";
+  } else {
+    myVideoStream.getAudioTracks()[0].enabled = true;
+    document.querySelector('.main__mute_button').innerHTML = "Mute";
+  }
+}
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    document.querySelector('.main__video_button').innerHTML = "Start Video";
+  } else {
+    myVideoStream.getVideoTracks()[0].enabled = true;
+    document.querySelector('.main__video_button').innerHTML = "Stop Video";
+  }
 }
--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer'); // Add this line
 const { v4: uuidV4 } = require('uuid');
 const path = require('path');
+
+// Set up the Peer Server inside your Node app
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/'
+});
+
+app.use('/peerjs', peerServer); // This is what the script.js looks for
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -29,6 +38,4 @@ io.on('connection', socket => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+server.listen(PORT);
